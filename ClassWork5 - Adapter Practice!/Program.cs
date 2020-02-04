a9f07cc70e3bb26738248de0003d13105daf0229
Matthew Pugh <matdpugh@ut.utm.edu>
2020-02-04 13:59:54 -0600

    Add project files.

---
+++ ClassWork5 - Adapter Practice!/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork5___Adapter_Practice_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
